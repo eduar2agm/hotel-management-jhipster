83c2b1e419da4ef19cf5d42d18b8cda7564663fe
3mrf <edgardo.ramos17@est.unanleon.edu.ni>
2025-12-11 22:26:09 -0600

    fix:autorización de enpoints

--- backend/src/main/java/com/hotel/app/config/OpenApiConfiguration.java
+++ backend/src/main/java/com/hotel/app/config/OpenApiConfiguration.java
@@ -1,5 +1,9 @@
 package com.hotel.app.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +16,15 @@ import tech.jhipster.config.apidoc.customizer.JHipsterOpenApiCustomizer;
 @Configuration
 @Profile(JHipsterConstants.SPRING_PROFILE_API_DOCS)
 public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                    new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
 
     public static final String API_FIRST_PACKAGE = "com.hotel.app.web.api";
 
