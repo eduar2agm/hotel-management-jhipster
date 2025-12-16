9406cb5a74d22e1672bcebe9e1a47e51694a039c
yosseling-ney <mayorgayosseling7@gmail.com>
2025-12-16 14:50:25 -0600

    ya tiene findByActivo() y findByActivoWithEagerRelationships() y para filtrado consistente en toda la aplicación

--- backend/src/main/java/com/hotel/app/repository/ReservaRepository.java
+++ backend/src/main/java/com/hotel/app/repository/ReservaRepository.java
@@ -38,4 +38,7 @@ public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     Page<Reserva> findByClienteId(Long clienteId, Pageable pageable);
 
     Page<Reserva> findByActivo(Boolean activo, Pageable pageable);
+
+    @Query("select distinct reserva from Reserva reserva left join fetch reserva.cliente where reserva.activo = :activo")
+    Page<Reserva> findByActivoWithEagerRelationships(@Param("activo") Boolean activo, Pageable pageable);
 }
