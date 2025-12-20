75bfa08ff72ea057a089812ef2965f26bb0bf894
yosseling-ney <mayorgayosseling7@gmail.com>
2025-12-20 09:47:03 -0600

    StaticResource guarda los combios

--- backend/src/main/java/com/hotel/app/config/StaticResourcesConfiguration.java
+++ backend/src/main/java/com/hotel/app/config/StaticResourcesConfiguration.java
@@ -1,6 +1,5 @@
 package com.hotel.app.config;
 
-import java.nio.file.Paths;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
