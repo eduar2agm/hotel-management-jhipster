a8137aaafdd7c21f7928bd81e1a61e9cace66ab2
eduar2agm <eduar2.agm2020@gmail.com>
2025-12-21 17:34:43 -0600

    feat(backend): agregar enum DiaSemana

---
+++ backend/src/main/java/com/hotel/app/domain/enumeration/DiaSemana.java
@@ -0,0 +1,14 @@
+package com.hotel.app.domain.enumeration;
+
+/**
+ * The DiaSemana enumeration.
+ */
+public enum DiaSemana {
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO,
+}
